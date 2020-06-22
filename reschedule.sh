b18b15a3fe0eb580be6ac2c2b1e8b969fb329f74
Igor Bezukh <ibezukh@redhat.com>
2020-06-22 18:48:33 +0300

    refactoring of scheduler

--- reschedule.sh
+++ reschedule.sh
@@ -6,8 +6,9 @@ log() { echo "$@" >&2; }
 
 patch_kubevirt_deployments() {
 	log "Patching Kubevirt deployments"
+	local YAML_FILE
 	YAML_FILE=$(mktemp -p /tmp -t kv_patch.XXX)
-	cat << EOF > $YAML_FILE
+	cat << EOF > "$YAML_FILE"
 spec:
   template:
     spec:
@@ -19,29 +20,31 @@ spec:
         node-role.kubernetes.io/master: ""
 EOF
 	for dpl in virt-api virt-controller; do
-		oc patch deployment $dpl --patch "$(cat $YAML_FILE)"
+		oc patch deployment $dpl --patch "$(cat "$YAML_FILE")"
 	done
 }
 
 patch_kubevirt_ds() {
 	log "Patching Kubevirt DaemonSets"
+	local YAML_FILE
 	YAML_FILE=$(mktemp -p /tmp -t kv_patch.XXX)
-	cat << EOF > $YAML_FILE
+	cat << EOF > "$YAML_FILE"
 spec:
   template:
     spec:
       nodeSelector:
         node-role.kubernetes.io/worker: ""
 EOF
-	oc patch ds virt-handler --patch "$(cat $YAML_FILE)"
+	oc patch ds virt-handler --patch "$(cat "$YAML_FILE")"
 }
 
 patch_cluster_service_version() {
 	log "Patching CNV Cluster Service Version for virt-operator deployment"
+	local CSV_NAME
+	local VIRT_OPERATOR_SEQ_NUM
 	CSV_NAME=$(oc get csv -n openshift-cnv -o custom-columns=:metadata.name)
-	VIRT_OPERATOR_SEQ_NUM=$(($(oc get csv $CSV_NAME -o=jsonpath='{range .spec.install.spec.deployments[*]}{.name}{"\n"}{end}' | grep -n "virt-operator" | cut -f1 -d:)-1))
-
-	oc patch csv ${CSV_NAME} -n openshift-cnv --type=json -p "[{'op': 'add','path': '/spec/install/spec/deployments/$VIRT_OPERATOR_SEQ_NUM/spec/template/spec/tolerations','value': [{'effect': 'NoSchedule','key': 'node-role.kubernetes.io/master','operator': 'Equal'}]},{'op': 'add','path': '/spec/install/spec/deployments/$VIRT_OPERATOR_SEQ_NUM/spec/template/spec/nodeSelector','value': {'node-role.kubernetes.io/master': ''}}]"
+	VIRT_OPERATOR_SEQ_NUM=$(($(oc get csv "$CSV_NAME" -o=jsonpath='{range .spec.install.spec.deployments[*]}{.name}{"\n"}{end}' | grep -n "virt-operator" | cut -f1 -d:)-1))
+	oc patch csv "${CSV_NAME}" -n openshift-cnv --type=json -p "[{'op': 'add','path': '/spec/install/spec/deployments/$VIRT_OPERATOR_SEQ_NUM/spec/template/spec/tolerations','value': [{'effect': 'NoSchedule','key': 'node-role.kubernetes.io/master','operator': 'Equal'}]},{'op': 'add','path': '/spec/install/spec/deployments/$VIRT_OPERATOR_SEQ_NUM/spec/template/spec/nodeSelector','value': {'node-role.kubernetes.io/master': ''}}]"
 }
 
 if [ -z "$(which oc)" ]; then

5b890456ec49743d8e9448c4578963a299a53633
Igor Bezukh <ibezukh@redhat.com>
2020-06-22 18:15:42 +0300

    refactoring of scheduler

--- reschedule.sh
+++ reschedule.sh
@@ -2,14 +2,12 @@
 
 set -e
 
-if [ -z $(which oc) ]; then
-       echo "OC tool is not installed"
-       exit 1
-fi
+log() { echo "$@" >&2; }
 
-rm -rf patch_taint.yaml patch_taint_worker.yaml patch_taint_csv.yaml
-
-cat << EOF > patch_taint.yaml
+patch_kubevirt_deployments() {
+	log "Patching Kubevirt deployments"
+	YAML_FILE=$(mktemp -p /tmp -t kv_patch.XXX)
+	cat << EOF > $YAML_FILE
 spec:
   template:
     spec:
@@ -20,33 +18,33 @@ spec:
       nodeSelector:
         node-role.kubernetes.io/master: ""
 EOF
+	for dpl in virt-api virt-controller; do
+		oc patch deployment $dpl --patch "$(cat $YAML_FILE)"
+	done
+}
 
-cat << EOF > patch_taint_worker.yaml
+patch_kubevirt_ds() {
+	log "Patching Kubevirt DaemonSets"
+	YAML_FILE=$(mktemp -p /tmp -t kv_patch.XXX)
+	cat << EOF > $YAML_FILE
 spec:
   template:
     spec:
       nodeSelector:
         node-role.kubernetes.io/worker: ""
 EOF
+	oc patch ds virt-handler --patch "$(cat $YAML_FILE)"
+}
 
-#Patch deployments
-for dpl in virt-api virt-controller; do
-	oc patch deployment $dpl --patch "$(cat patch_taint.yaml)"
-done
+patch_cluster_service_version() {
+	log "Patching CNV Cluster Service Version for virt-operator deployment"
+	CSV_NAME=$(oc get csv -n openshift-cnv -o custom-columns=:metadata.name)
+	VIRT_OPERATOR_SEQ_NUM=$(($(oc get csv $CSV_NAME -o=jsonpath='{range .spec.install.spec.deployments[*]}{.name}{"\n"}{end}' | grep -n "virt-operator" | cut -f1 -d:)-1))
 
-#Patch DaemonSets
-for dst in virt-handler; do
-	oc patch ds $dst --patch "$(cat patch_taint_worker.yaml)"
-done
+	oc patch csv ${CSV_NAME} -n openshift-cnv --type=json -p "[{'op': 'add','path': '/spec/install/spec/deployments/$VIRT_OPERATOR_SEQ_NUM/spec/template/spec/tolerations','value': [{'effect': 'NoSchedule','key': 'node-role.kubernetes.io/master','operator': 'Equal'}]},{'op': 'add','path': '/spec/install/spec/deployments/$VIRT_OPERATOR_SEQ_NUM/spec/template/spec/nodeSelector','value': {'node-role.kubernetes.io/master': ''}}]"
+}
 
-##Patch ClusterServiceVersion
-
-# get the CNV's CSV name
-CSV_NAME=$(oc get csv -n openshift-cnv -o custom-columns=:metadata.name)
-
-# get the virt-operator deployment sequence number in the CSV (subtracting 1 as it's starts from 0)
-VIRT_OPERATOR_SEQ_NUM=$(($(oc get csv $CSV_NAME -o=jsonpath='{range .spec.install.spec.deployments[*]}{.name}{"\n"}{end}' | grep -n "virt-operator" | cut -f1 -d:)-1))
-
-# patch the virt-operator deployment with the tolerations and node selector
-oc patch csv ${CSV_NAME} -n openshift-cnv --type=json -p "[{'op': 'add','path': '/spec/install/spec/deployments/$VIRT_OPERATOR_SEQ_NUM/spec/template/spec/tolerations','value': [{'effect': 'NoSchedule','key': 'node-role.kubernetes.io/master','operator': 'Equal'}]},{'op': 'add','path': '/spec/install/spec/deployments/$VIRT_OPERATOR_SEQ_NUM/spec/template/spec/nodeSelector','value': {'node-role.kubernetes.io/master': ''}}]"
-
+if [ -z "$(which oc)" ]; then
+       echo "OC tool is not installed"
+       exit 1
+fi
